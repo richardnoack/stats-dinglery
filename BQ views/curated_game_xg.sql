42de80eaaadf5a9dbd7dfdb739b8a1e33cd43d80
Richard Noack <34106835+richardnoack@users.noreply.github.com>
2022-03-23 10:21:57 -0300

    dale

--- BQ views/curated_game_xg.sql
+++ BQ views/curated_game_xg.sql
@@ -6,11 +6,11 @@ game_id
 , date_time_utc
 , home_team_id as team_id
 , t.team_name
-, t.team_short_name
+, t.team_abbreviation as team_short_name
 , ROUND(home_player_xgoals,2) as xgf
 , away_team_id as opp_id
 , t2.team_name as opp_name
-, t2.team_short_name as opp_short_name
+, t2.team_abbreviation as opp_short_name
 , ROUND(away_player_xgoals,2) as xga
 , ROUND(player_xgoal_difference,2) as xgd
 , ROUND(home_xpoints,2) as xpoints
@@ -27,11 +27,11 @@ game_id
 , date_time_utc
 , away_team_id as team_id
 , t2.team_name 
-, t2.team_short_name
+, t2.team_abbreviation as team_short_name
 , ROUND(away_player_xgoals,2) as xgf
 , home_team_id as opp_id
 , t.team_name as opp_name
-, t.team_short_name as opp_short_name
+, t.team_abbreviation as opp_short_name
 , ROUND(home_player_xgoals,2) as xga
 , ROUND(player_xgoal_difference*-1,2) as xgd
 , ROUND(away_xpoints,2) as xpoints
---
+++ BQ views/ratings_exploration.sql
@@ -0,0 +1,108 @@
+##### Another version where H/A is split
+### this might be simpler when you join to schedule on location + team
+WITH ratings as 
+(SELECT 
+team_short_name
+, location
+### xgF ratings offense
+, AVG(xgf) as xgf
+### xgA ratings defense
+, AVG(xga) as xga
+, COUNT(distinct game_id) as sample_size
+
+FROM curated.game_xg_view
+WHERE EXTRACT(YEAR FROM DATE(date_time_utc)) > 2020
+AND team_short_name in ('SKC', 'LAFC')
+GROUP BY 1,2)
+
+SELECT 
+s.team_name
+, s.short_name
+, s.round
+, s.location
+, s.opp_short_name 
+, r.xga as opp_defensive_rating
+, r.xgf as opp_offensive_rating
+from curated.schedule s
+LEFT JOIN ratings r ON
+r.team_short_name = s.opp_short_name 
+AND s.location != r.location ###makes sure locations are inverted
+WHERE round BETWEEN 3 AND 10 ## between is inclusive of both ends 
+AND short_name = 'VAN'
+
+### scenario VAN plays at LAFC: 
+### opp def rating is 0.97 and opp off rating is 2.05 <- meaning LAFC averages 2.05 xGF at home
+
+### VAN hosts SKC: 
+### off: 1.51  def: 1.09
+SELECT 
+team_short_name
+, location
+### xgF ratings offense
+, AVG(xgf) as xgf
+### xgA ratings defense
+, AVG(xga) as xga
+, COUNT(distinct game_id) as sample_size
+
+FROM curated.game_xg_view
+WHERE EXTRACT(YEAR FROM DATE(date_time_utc)) > 2020
+AND team_short_name in ('SKC', 'LAFC')
+GROUP BY 1,2
+### checks out
+
+
+###### actual query for next 5 ratings 
+DECLARE next_round DEFAULT (SELECT min(round) from curated.schedule WHERE simple_date >= CURRENT_DATE());
+
+CREATE OR REPLACE TABLE curated.difficulty_ratings as
+### round selector 
+
+
+WITH ratings as 
+(SELECT 
+team_short_name
+, location
+### xgF ratings offense
+, AVG(xgf) as xgf
+### xgA ratings defense
+, AVG(xga) as xga
+, COUNT(distinct game_id) as sample_size
+
+FROM curated.game_xg_view
+WHERE EXTRACT(YEAR FROM DATE(date_time_utc)) > 2020
+GROUP BY 1,2
+--ORDER BY 3 desc
+)
+
+, intermediate as
+(
+SELECT 
+s.team_name
+, s.short_name
+, s.round
+, s.location
+, s.opp_short_name 
+, r.xga as opp_xga
+, r.xgf as opp_xgf
+, s.date
+from curated.schedule s
+LEFT JOIN ratings r ON
+r.team_short_name = s.opp_short_name 
+AND s.location != r.location ###makes sure locations are inverted
+WHERE round BETWEEN 
+###round_selector
+next_round
+AND next_round+5 ## between is inclusive of both ends 
+)
+
+SELECT 
+team_name
+, short_name
+, COUNT(1) as num_games
+, ROUND(100*COUNT(IF(location = 'home', 1, null))/COUNT(1),0) as perc_home_games
+, ROUND(AVG(opp_xga),2) as avg_opp_xga ###higher is better  , it's how many xG the opp give up
+, ROUND(AVG(opp_xgf),2) as avg_opp_xgf ### lower is better, it's the avg xGF of the opps
+, STRING_AGG(IF(location = 'home', UPPER(opp_short_name), LOWER(opp_short_name)), ' | ' ORDER BY date ASC) as next_opps
+FROM intermediate
+GROUP BY 1,2
+ORDER BY avg_opp_xgf 
